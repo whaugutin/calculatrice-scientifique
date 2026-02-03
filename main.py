43639b02305e1284a1becfbb4721888118f789d7
whaugutin <whitchyaugustin@gmail.com>
2026-02-03 10:50:54 -0500

    Control de Saisie

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    nbr_1 = input("Entrer le premier nombre: ")
-    nbr_2 = input("Entrer le deuxième nombre: ")
+def verify_number(nbr):
+    while True:
+        try:
+            float(nbr)
+            return nbr
+        except ValueError:
+            nbr = input("Entrée invalide. Veuillez entrer un nombre valide: ")
+
+def main():    
+    nbr_1 = verify_number(input("Entrer le premier nombre: "))
+    nbr_2 = verify_number(input("Entrer le deuxième nombre: "))
 
     print("Le résultat de l'addition est: ", float(nbr_1) + float(nbr_2))
 

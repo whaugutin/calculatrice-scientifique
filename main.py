1262327b4e112db42103a08d2ebd09ad40341b3b
whaugutin <whitchyaugustin@gmail.com>
2026-02-03 10:37:38 -0500

    Implemention of the code to add up two numbers

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def main():
-    print("Hello from calculatrice-scientifique!")
+    nbr_1 = input("Entrer le premier nombre: ")
+    nbr_2 = input("Entrer le deuxième nombre: ")
+
+    print("Le résultat de l'addition est: ", float(nbr_1) + float(nbr_2))
 
 
 if __name__ == "__main__":
